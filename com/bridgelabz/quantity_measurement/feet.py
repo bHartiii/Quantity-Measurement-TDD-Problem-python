4150041be4dbd1ad8789714a27883d9f02c4c3e4
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-09 00:18:04 +0530

    Bharti-ADD : Classes for all length type are created

---
+++ com/bridgelabz/quantity_measurement/feet.py
@@ -0,0 +1,8 @@
+class Feet:
+
+    def __init__(self, feet):
+        self.feet = feet
+
+    def __eq__(self, other):
+        return self.feet == other
+
---
+++ com/bridgelabz/quantity_measurement/yard.py
@@ -0,0 +1,6 @@
+class Yard:
+    def __init__(self, yard):
+        self.yard = yard
+
+    def __eq__(self, other):
+        return self.yard == other
