42ad9c625b6c28d40cc0164abaf99ba96f5200dd
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-10 02:54:01 +0530

    Bharti-REFACTOR : Condition for addition of 1ft and 2in is added

--- com/bridgelabz/quantity_measurement/length_adder.py
+++ com/bridgelabz/quantity_measurement/length_adder.py
@@ -9,3 +9,5 @@ class LengthAdder:
     def add_length(self):
         if self.object.convertor == Lengths.Self_To_Self.name:
             return self.object.value1 + self.object.value2
+        if self.object.convertor == Lengths.Feet_To_Inch.name:
+            return self.object.value1 * Lengths.Feet_To_Inch.value + self.object.value2
