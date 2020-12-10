669dc8a934c2a12875e0c4c61800a92f77fcf4eb
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-11 00:03:28 +0530

    Bharti-ADD : Test cases are added for inch to centimeter comparison

--- com/bridgelabz/quantity_measurement/length_type.py
+++ com/bridgelabz/quantity_measurement/length_type.py
@@ -2,12 +2,13 @@ import enum
 
 
 class Lengths(enum.Enum):
-    Self_To_Self = 1
-    Inch_To_Feet = 1/12
-    Feet_To_Inch = 12
-    Yard_To_Feet = 3
-    Feet_To_Yard = 1/3
-    Yard_To_Inch = 36
-    Inch_To_Yard = 1/36
-    Inch_To_Centimeter = 5/2
-    Centimeter_To_Inch = 2/5
+    INCH = 1
+    FEET = 12
+    CM = 0.4
+    YARD = 36
+
+    def __init__(self, unit):
+        self.unit = unit
+
+    def convert_into_inch(self, value):
+        return self.unit * value
