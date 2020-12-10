fa5335bd0d4d1b0605f32cc8963a48d62d876739
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-10 23:31:58 +0530

    Bharti-REFACTOR : Code and test cases are refactored to do conversion on into base unit

---
+++ com/bridgelabz/quantity_measurement/quantity_measurement.py
@@ -0,0 +1,15 @@
+from com.bridgelabz.quantity_measurement.length_type import Lengths
+
+
+class LengthConvertor:
+
+    def __init__(self, value, ob):
+        self.value = value
+        self.ob = ob
+
+    def __eq__(self, other):
+        if isinstance(other, LengthConvertor):
+            if self.ob.convert_into_inch(self.value) == other.ob.convert_into_inch(other.value):
+                return True
+            else:
+                return False
